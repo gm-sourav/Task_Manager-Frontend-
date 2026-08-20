0f824b5d7220e21f9e9606a4c2f4def5d140ff8d
gm-sourav <souravmahbub13@gmail.com>
2026-08-20 23:18:09 +0600

    first

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
 
+
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
@@ -55,4 +56,5 @@ dependencies {
     // Lifecycle - ViewModel use korার jonno
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+
 }
