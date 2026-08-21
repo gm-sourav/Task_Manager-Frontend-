46324b37f72c75a1b2ada07abf2e9ee6df597705
gm-sourav <souravmahbub13@gmail.com>
2026-08-21 11:30:25 +0600

    first

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.firebase.appdistribution.gradle)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
