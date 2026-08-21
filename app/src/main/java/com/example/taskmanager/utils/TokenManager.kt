61c27b0160b7beee8c67eb4b8abcc13d7c9a11cc
gm-sourav <souravmahbub13@gmail.com>
2026-08-21 11:59:45 +0600

    second

--- app/src/main/java/com/example/taskmanager/utils/TokenManager.kt
+++ app/src/main/java/com/example/taskmanager/utils/TokenManager.kt
@@ -1,4 +1,29 @@
 package com.example.taskmanager.utils
 
-class TokenManager {
+import android.content.Context
+import android.content.SharedPreferences
+
+class TokenManager(context: Context) {
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences("TaskManagerPrefs", Context.MODE_PRIVATE)
+
+    companion object{
+        private const val  KEY_TOKEN = "jwt_token"
+    }
+
+    fun saveToken(token: String){
+        prefs.edit().putString(KEY_TOKEN, token).apply()
+    }
+
+    fun getToken(): String? {
+        return prefs.getString(KEY_TOKEN, null)
+    }
+
+    fun clearToken(){
+        prefs.edit().remove(KEY_TOKEN).apply()
+    }
+
+    fun isoLoggedIn(): Boolean {
+        return getToken() != null
+    }
 }
