25ac8d2669bd70fd2bbb4ba8f4beee1ea947a924
gm-sourav <souravmahbub13@gmail.com>
2026-08-22 23:27:02 +0600

    nd

--- app/src/main/java/com/example/taskmanager/network/ApiService.kt
+++ app/src/main/java/com/example/taskmanager/network/ApiService.kt
@@ -9,6 +9,7 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.HTTP
 import retrofit2.http.Header
 import retrofit2.http.PATCH
 import retrofit2.http.POST
@@ -48,7 +49,7 @@ interface ApiService {
     ): Response<TaskResponse>
 
 
-    @PATCH("api/tasks/{id}/status")
+    @HTTP(method = "api/tasks/{id}/status")
     suspend fun updateStatus(
         @Header("authorization") token: String,
         @Path("id") taskId: Long,
@@ -57,8 +58,8 @@ interface ApiService {
 
     @DELETE("api/tasks/{id}")
     suspend fun deleteTask(
-        @Header("authorization") token: String,
+        @Header("Authorization") token: String,
         @Path("id") taskId: Long
-    ): Response<String>
+    ): Response<Unit>
 
 }
